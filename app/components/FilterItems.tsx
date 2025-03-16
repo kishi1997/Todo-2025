636caf9be9dece48db7d486608cfe9e3ee0fbc19
kishi1997 <tomo9634@gmail.com>
2025-03-16 02:16:09 -0700

    タスクのフィルタリング機能を追加し、FilterItemsコンポーネントにフィルタリング用のボタンを実装。初期Todoリストの状態管理を追加し、APIから取得したTodoリストを基に表示を更新する処理を改善。

--- app/components/FilterItems.tsx
+++ app/components/FilterItems.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
-
-const FilterItems = () => {
+interface FilterItemsProps {
+  setFilterdTodoList: (status: string) => void;
+}
+const FilterItems = ({ setFilterdTodoList }: FilterItemsProps) => {
+  const showAllTasks = () => {
+    setFilterdTodoList('all');
+  };
+  const showUncompleteTasks = () => {
+    setFilterdTodoList('uncomplete');
+  };
+  const showCompleteTasks = () => {
+    setFilterdTodoList('complete');
+  };
   return (
     <div className="mt-6 flex justify-between items-center border-t border-gray-700 pt-4">
       <div className="text-sm text-gray-400">「」件の未完了タスク</div>
       <div className="flex space-x-2">
-        <button className="placeholder:px-3 py-1 rounded-md text-xs">すべて</button>
-        <button className="px-3 py-1 rounded-md text-xs">未完了</button>
-        <button className="px-3 py-1 rounded-md text-xs">完了済み</button>
+        <button onClick={showAllTasks} className="placeholder:px-3 py-1 rounded-md text-xs">
+          すべて
+        </button>
+        <button onClick={showUncompleteTasks} className="px-3 py-1 rounded-md text-xs">
+          未完了
+        </button>
+        <button onClick={showCompleteTasks} className="px-3 py-1 rounded-md text-xs">
+          完了済み
+        </button>
       </div>
     </div>
   );
