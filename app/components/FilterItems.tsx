bdb27635f24da2d559434e3a4b8e07d10d57bb0f
kishi1997 <tomo9634@gmail.com>
2025-03-16 18:44:46 -0700

    FilterItemsコンポーネントに初期Todoリストを追加し、未完了タスクの数を表示する機能を実装。propsの使用方法を修正。

--- app/components/FilterItems.tsx
+++ app/components/FilterItems.tsx
@@ -1,20 +1,29 @@
 import React from 'react';
-interface FilterItemsProps {
+import { Todo } from '../types/type';
+
+type FilterItemsProps = {
+  initialTodoList: Todo[] | null;
   setFilterdTodoList: (status: string) => void;
-}
-const FilterItems = ({ setFilterdTodoList }: FilterItemsProps) => {
+};
+const FilterItems = (props: FilterItemsProps) => {
   const showAllTasks = () => {
-    setFilterdTodoList('all');
+    props.setFilterdTodoList('all');
   };
   const showUncompleteTasks = () => {
-    setFilterdTodoList('uncomplete');
+    props.setFilterdTodoList('uncomplete');
   };
   const showCompleteTasks = () => {
-    setFilterdTodoList('complete');
+    props.setFilterdTodoList('complete');
+  };
+  // 未完成のタスク数取得
+  const getUnCompleteTaskNumber = () => {
+    if (props.initialTodoList == null) return 0;
+    const UnCompleteTaskList = props.initialTodoList.filter((todo) => todo.complete !== true);
+    return UnCompleteTaskList.length;
   };
   return (
     <div className="mt-6 flex justify-between items-center border-t border-gray-700 pt-4">
-      <div className="text-sm text-gray-400">「」件の未完了タスク</div>
+      <div className="text-sm text-gray-400">{getUnCompleteTaskNumber()}件の未完了タスク</div>
       <div className="flex space-x-2">
         <button onClick={showAllTasks} className="placeholder:px-3 py-1 rounded-md text-xs">
           すべて
