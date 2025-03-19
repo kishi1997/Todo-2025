3667b94a5f5e81e249ac0d430a5447acfc5c05ad
kishi1997 <tomo9634@gmail.com>
2025-03-19 02:28:39 -0700

    コンポーネント内のインポートパスを修正し、'Todostore'から'TodoStore'に変更。これにより、コードの一貫性を向上。

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React, { useEffect } from 'react';
-import { useTodoStore } from '../store/Todostore';
+import { useTodoStore } from '../store/TodoStore';
 
 interface ModalProps {
   open: boolean;
